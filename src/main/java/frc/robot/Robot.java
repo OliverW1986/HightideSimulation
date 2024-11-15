154252ea4a8e19a1361c667d769c6205e6fe597a
Oliver Weller <ImpactTang@outlook.com>
2024-11-15 01:08:20 -0600

    Updated Poses

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import dev.doglog.DogLog;
 import dev.doglog.DogLogOptions;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,7 +29,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
     DogLog.log("Drive/ZeroedPose", new Pose3d());
-    DogLog.log("Drive/ComponentZeroedPoses", new Pose3d[] { new Pose3d(), new Pose3d(), new Pose3d() });
+    DogLog.log("Drive/ComponentZeroedPoses", new Pose3d[] { new Pose3d(), new Pose3d(0.5, 0.0, 0.0, new Rotation3d(0, 0.0, 0)), new Pose3d(0.0, 0.0, 0.0, new Rotation3d()) });
   }
 
   @Override
